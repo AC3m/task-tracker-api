4d3ac646de755cef873ee09c0dfa031a311a7b77
Artur Cempura <artur.cempura@grandparade.co.uk>
2026-01-07 16:45:13 +0100

    feat: Integrate delete route with DB

--- src/routes/tasks.ts
+++ src/routes/tasks.ts
@@ -6,13 +6,22 @@ const router = express.Router();
 
 const taskCollection = db.collection('tasks');
 
+interface Task {
+  id?: string;
+  task: string;
+  done?: boolean;
+}
+
 //TO DO: Add controllers
 
 router.get('/', async (req, res, next) => {
   try {
     const snapshot = await taskCollection.get();
-    const tasks = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
-    res.status(200).json(tasks);
+    const tasksCollection = snapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+    res.status(200).json(tasksCollection);
   } catch (error) {
     next(error);
   }
@@ -21,8 +30,8 @@ router.get('/', async (req, res, next) => {
 router.get('/:id', async (req, res, next) => {
   try {
     const { id } = req.params;
-    const doc = await taskCollection.doc(id).get();
-    if (!doc.exists) {
+    const taskData = await taskCollection.doc(id).get();
+    if (!taskData.exists) {
       const error = new Error(
         `A task with the id of ${id} was not found`,
       ) as CustomError;
@@ -30,10 +39,10 @@ router.get('/:id', async (req, res, next) => {
       return next(error);
     }
     res.status(200).json({
-      id: doc.id,
-      task: doc.data()?.task,
-      done: doc.data()?.done,
-      createdAt: doc.data()?.createdAt,
+      id: taskData.id,
+      task: taskData.data()?.task,
+      done: taskData.data()?.done,
+      createdAt: taskData.data()?.createdAt,
     });
   } catch (error) {
     next(error);
@@ -41,24 +50,24 @@ router.get('/:id', async (req, res, next) => {
 });
 
 router.post('/', async (req, res, next) => {
-  const { task } = req.body;
-  if (!task) {
-    const error = new Error('Bad Request') as CustomError;
-    error.status = 400;
-    next(error);
-  }
-
   try {
+    const { task } = req.body;
+    if (!task) {
+      const error = new Error('Bad Request') as CustomError;
+      error.status = 400;
+      next(error);
+    }
+
     const snapshot = await taskCollection.get();
     const newId = (snapshot.size + 1).toString();
-    const newTask = {
+    const newTaskData = {
       task,
       done: false,
       createdAt: new Date(),
     };
 
-    await taskCollection.doc(newId).set(newTask);
-    res.status(201).json({ id: newId, ...newTask });
+    await taskCollection.doc(newId).set(newTaskData);
+    res.status(201).json({ id: newId, ...newTaskData });
   } catch (error) {
     next(error);
   }
@@ -68,20 +77,20 @@ router.put('/:id', async (req, res, next) => {
   try {
     const { id } = req.params;
     const taskRef = taskCollection.doc(id);
-    const doc = await taskRef.get();
-    if (!doc.exists) {
+    const taskData = await taskRef.get();
+    if (!taskData.exists) {
       const error = new Error(
         `A task with the id of ${id} was not found`,
       ) as CustomError;
       error.status = 404;
       return next(error);
     }
-    const currentData = doc.data();
+    const currentData = taskData.data();
     const newStatus = !currentData?.done;
 
     await taskRef.update({ done: newStatus });
     res.status(200).json({
-      id: doc.id,
+      id: taskData.id,
       ...currentData,
       done: newStatus,
     });
@@ -90,21 +99,31 @@ router.put('/:id', async (req, res, next) => {
   }
 });
 
-// TODO: Refactor delete route to use Firestore
-
-// router.delete('/:id', (req, res, next) => {
-//   const { id: paramId } = req.params;
-//   const task = taskList.find((task) => task.id === +paramId);
-//   if (!task) {
-//     // const error = { error: `A task with the id of ${paramId} was not found` };
-//     const error = new Error(
-//       `A task with the id of ${paramId} was not found`,
-//     ) as CustomError;
-//     error.status = 404;
-//     return next(error);
-//   }
-//   const updatedTaskList = taskList.filter((task) => task.id !== +paramId);
-//   res.status(200).json(updatedTaskList);
-// });
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const taskRef = taskCollection.doc(id);
+    const taskData = await taskRef.get();
+    if (!taskData.exists) {
+      const error = new Error(
+        `A task with the id of ${id} was not found`,
+      ) as CustomError;
+      error.status = 404;
+      return next(error);
+    }
+    await taskRef.delete();
+    const snapshot = await taskCollection.get();
+    const updatedCollection: Task[] = snapshot.docs.map(
+      (task) =>
+        ({
+          id: task.id,
+          ...task.data(),
+        }) as Task,
+    );
+    res.status(200).json(updatedCollection);
+  } catch (error) {
+    next(error);
+  }
+});
 
 export default router;
